6898b0dc1d7eeccad5cae8520b7b89deaf1e9600
Terence K <ikke@botnet.media.local>
2015-08-25 12:25:57 +0200

    Initial

---
+++ app.js
@@ -0,0 +1,99 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var http = require('http');
+
+var redis = require('redis');
+var client = redis.createClient();
+///////////////
+//
+//
+//
+//
+///////////////
+
+var GROUP_COUNT = 9;
+
+client.set('GROUP_COUNT', GROUP_COUNT);
+
+client.hexists('group:'+GROUP_COUNT, 'logo', function(err, reply) {
+    if (err)
+        throw err;
+
+    if (reply === 1) return;
+
+    for (var i = 1; i < GROUP_COUNT+1; i++) {
+        client.hmset('group:'+i, {
+            logo: 'http://placehold.it/100x100',
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            url: "http://example.com/",
+            members: "Member1,Member2",
+        });
+    }
+});
+
+//////////////
+
+var routes = require('./routes/index');
+var groups = require('./routes/groups');
+var manage = require('./routes/manage');
+
+var app = express();
+
+app.set('port', 3000);
+
+var server = http.createServer(app);
+server.listen(3000);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/groups', groups);
+app.use('/manage', manage);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
---
+++ routes/groups.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var redis = require('redis');
+
+var router = express.Router();
+var client = redis.createClient();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    client.get('GROUP_COUNT', function(err, val){
+        if (err)
+            throw err;
+
+        var objs = [];
+        var left = parseInt(val);
+        for (var i = 1;  i < left+1; i++) {
+            client.hmget('group:'+i, 'logo', 'description', 'url', 'members', function(err, obj) {
+                objs.push({
+                    logo: obj[0],
+                    description: obj[1],
+                    url: obj[2],
+                    members: obj[3].split(","),
+                });         
+                left--;
+                check();
+            });
+        }
+        function check() {
+            if (left > 0) return;
+            objs.forEach(function(obj, index, array) {
+                obj.name = "Groep " + (index+1);
+            });
+            res.render('groups', {
+                title: "Groepen",
+                groups: objs,
+            });
+        }
+    });
+});
+
+module.exports = router;
